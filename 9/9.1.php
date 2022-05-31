ecb41213a7ee2088e7624d6cc50490f32434006c
drmcastles <drmcast@yandex.ru>
2022-05-31 17:44:26 +0300

    Глава 9

---
+++ 9/9.1.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+ switch ($_GET['message']){
+     case "test1":
+         echo "1";
+         break;
+     case "test2":
+         echo "2";
+         break;
+     case "test5":
+         echo "3";
+         break;
+     default:
+         echo "4";
+ }
